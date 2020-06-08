6f310a4cda4eac655ab5bef188f47a4215bf7551
francesca-campana <franx3rox@gmail.com>
2020-06-08 14:32:01 +0200

    full

---
+++ main.js
@@ -0,0 +1,13 @@
+
+$ ('.header-right > a').click(
+  function() {
+     $ ('div.hamburger-menu').show();
+  }
+);
+
+
+ $ ('.close').click(
+   function() {
+     $ ('div.hamburger-menu').hide()
+   }
+ );
